2e78409c4fc9631fcd6f6a4b305c03ac6348ec66
Dmitry Goncharov <notesmobile@gmail.com>
2025-12-22 01:38:19 +0300

    Added hw07

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,91 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
+	ErrNegativeOffset        = errors.New("negative offset")
+	ErrNegativeLimit         = errors.New("negative limit")
+	ErrInvalidDestPath       = errors.New("invalid destination path")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if offset < 0 {
+		return ErrNegativeOffset
+	}
+	if limit < 0 {
+		return ErrNegativeLimit
+	}
+	if fromPath == toPath {
+		return ErrInvalidDestPath
+	}
+
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err := sourceFile.Close()
+		if err != nil {
+			fmt.Println("Error on close source file", err)
+		}
+	}()
+
+	fi, err := sourceFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if fi.IsDir() || !fi.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fi.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	destFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err := destFile.Close()
+		if err != nil {
+			fmt.Println("Error on close destination file", err)
+		}
+	}()
+
+	if offset > 0 {
+		_, err = sourceFile.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	var total int64
+	if limit == 0 || limit > fi.Size()-offset {
+		total = fi.Size() - offset
+	} else {
+		total = limit
+	}
+
+	bar := pb.Full.Start64(total)
+
+	barReader := bar.NewProxyReader(sourceFile)
+
+	_, err = io.CopyN(destFile, barReader, total)
+
+	bar.Finish()
+
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,156 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	tests := []struct {
+		name   string
+		from   string
+		to     string
+		offset int64
+		limit  int64
+		err    error
+		exp    string
+	}{
+		{
+			name:   "negative offset",
+			from:   "./testdata/input.txt",
+			to:     "./out.txt",
+			offset: -10,
+			limit:  0,
+			err:    ErrNegativeOffset,
+			exp:    "./testdata/out_offset0_limit0.txt",
+		},
+		{
+			name:   "negative limit",
+			from:   "./testdata/input.txt",
+			to:     "./out.txt",
+			offset: 0,
+			limit:  -10,
+			err:    ErrNegativeLimit,
+			exp:    "./testdata/out_offset0_limit0.txt",
+		},
+		{
+			name:   "invalid source path",
+			from:   "./testdata/input1.txt",
+			to:     "./testdata/input.txt",
+			offset: 0,
+			limit:  0,
+			err:    os.ErrNotExist,
+			exp:    "./testdata/out_offset0_limit0.txt",
+		},
+		{
+			name:   "invalid destination path",
+			from:   "./testdata/input.txt",
+			to:     "./testdata/input.txt",
+			offset: 0,
+			limit:  0,
+			err:    ErrInvalidDestPath,
+			exp:    "./testdata/out_offset0_limit0.txt",
+		},
+		{
+			name:   "unsupport dir",
+			from:   "./testdata",
+			to:     "./out_offset0_limit0.txt",
+			offset: 0,
+			limit:  0,
+			err:    ErrUnsupportedFile,
+			exp:    "./testdata/out_offset0_limit0.txt",
+		},
+		{
+			name:   "undefined file size",
+			from:   "/dev/urandom",
+			to:     "./out_offset0_limit0.txt",
+			offset: 0,
+			limit:  0,
+			err:    ErrUnsupportedFile,
+			exp:    "./testdata/out_offset0_limit0.txt",
+		},
+		{
+			name:   "offset exceeds file size",
+			from:   "./testdata/input.txt",
+			to:     "./out_offset0_limit0.txt",
+			offset: 1000000000,
+			limit:  0,
+			err:    ErrOffsetExceedsFileSize,
+			exp:    "./testdata/out_offset0_limit0.txt",
+		},
+		{
+			name:   "offset 0 limit 0",
+			from:   "./testdata/input.txt",
+			to:     "./out_offset0_limit0.txt",
+			offset: 0,
+			limit:  0,
+			err:    nil,
+			exp:    "./testdata/out_offset0_limit0.txt",
+		},
+		{
+			name:   "offset 0 limit 10",
+			from:   "./testdata/input.txt",
+			to:     "./out_offset0_limit10.txt",
+			offset: 0,
+			limit:  10,
+			err:    nil,
+			exp:    "./testdata/out_offset0_limit10.txt",
+		},
+		{
+			name:   "offset 0 limit 1000",
+			from:   "./testdata/input.txt",
+			to:     "./out_offset0_limit1000.txt",
+			offset: 0,
+			limit:  1000,
+			err:    nil,
+			exp:    "./testdata/out_offset0_limit1000.txt",
+		},
+		{
+			name:   "offset 0 limit 10000",
+			from:   "./testdata/input.txt",
+			to:     "./out_offset0_limit10000.txt",
+			offset: 0,
+			limit:  10000,
+			err:    nil,
+			exp:    "./testdata/out_offset0_limit10000.txt",
+		},
+		{
+			name:   "offset 100 limit 1000",
+			from:   "./testdata/input.txt",
+			to:     "./out_offset100_limit1000.txt",
+			offset: 100,
+			limit:  1000,
+			err:    nil,
+			exp:    "./testdata/out_offset100_limit1000.txt",
+		},
+		{
+			name:   "offset 6000 limit 1000",
+			from:   "./testdata/input.txt",
+			to:     "./out_offset6000_limit1000.txt",
+			offset: 6000,
+			limit:  1000,
+			err:    nil,
+			exp:    "./testdata/out_offset6000_limit1000.txt",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			err := Copy(test.from, test.to, test.offset, test.limit)
+
+			if test.err != nil {
+				require.ErrorIs(t, err, test.err)
+			} else {
+				require.NoError(t, err)
+
+				fiExp, _ := os.Stat(test.exp)
+				fiAct, _ := os.Stat(test.to)
+				require.Equal(t, fiExp.Size(), fiAct.Size())
+
+				os.Remove(test.to)
+			}
+		})
+	}
 }
