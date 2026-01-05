9b725bd74d8739a34e389a2b04c8efffb5db6745
Dmitry Goncharov <notesmobile@gmail.com>
2026-01-05 17:53:51 +0300

    Added hw11

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,59 @@
 package main
 
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	var timeout time.Duration
+	flag.DurationVar(&timeout, "timeout", 10*time.Second, "timeout connection")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 2 {
+		fmt.Println("Should be set host and port")
+		os.Exit(1)
+	}
+
+	client := NewTelnetClient(net.JoinHostPort(args[0], args[1]), timeout, os.Stdin, os.Stdout)
+
+	err := client.Connect()
+	if err != nil {
+		fmt.Println("Can't open connection:", err.Error())
+		os.Exit(1)
+	}
+
+	defer func() {
+		err := client.Close()
+		if err != nil {
+			fmt.Println("Can't close connection:", err.Error())
+		}
+	}()
+
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT)
+	defer cancel()
+
+	go func() {
+		err := client.Send()
+		if err != nil {
+			fmt.Println("Can't send data:", err.Error())
+		}
+		cancel()
+	}()
+
+	go func() {
+		err := client.Receive()
+		if err != nil {
+			fmt.Println("Can't receive data:", err.Error())
+		}
+		cancel()
+	}()
+
+	<-ctx.Done()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -13,9 +16,63 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &TelnetClientSimple{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+		conn:    nil,
+	}
+}
+
+type TelnetClientSimple struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
+var errNoConnection = fmt.Errorf("no connection")
+
+func (c *TelnetClientSimple) Connect() error {
+	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
+	defer cancel()
+
+	dialer := &net.Dialer{}
+	conn, err := dialer.DialContext(ctx, "tcp", c.address)
+	if err != nil {
+		return fmt.Errorf("error connection: %w", err)
+	}
+	c.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *TelnetClientSimple) Close() error {
+	if c.conn == nil {
+		return errNoConnection
+	}
+	return c.conn.Close()
+}
+
+func (c *TelnetClientSimple) Send() error {
+	if c.conn == nil {
+		return errNoConnection
+	}
+	_, err := io.Copy(c.conn, c.in)
+	if err != nil {
+		return fmt.Errorf("error sending data: %w", err)
+	}
+	return nil
+}
+
+func (c *TelnetClientSimple) Receive() error {
+	if c.conn == nil {
+		return errNoConnection
+	}
+	_, err := io.Copy(c.out, c.conn)
+	if err != nil {
+		return fmt.Errorf("error receiving data: %w", err)
+	}
+	return nil
+}
