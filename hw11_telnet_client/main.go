dd9366c424070c5eecb745e40b1844aec9429555
Dmitry Goncharov <notesmobile@gmail.com>
2026-01-06 22:42:21 +0300

    Do not cancel on error send

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -30,7 +30,7 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	defer cancel()
 
-	go send(client, cancel)
+	go send(client)
 	go receive(client)
 
 	<-ctx.Done()
@@ -57,11 +57,10 @@ func parseConf() (*conf, error) {
 	}, nil
 }
 
-func send(client TelnetClient, cancel context.CancelFunc) {
+func send(client TelnetClient) {
 	err := client.Send()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Can't send data:", err.Error())
-		cancel()
 	}
 }
 
